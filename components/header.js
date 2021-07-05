c9dbc2277b2fb0a6663072a03befb5179f01775f
z27fang <xhfangzihao@gmail.com>
2021-07-05 14:10:13 -0400

    fix: fix header padding

--- components/header.js
+++ components/header.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 function MenuLink(props) {
     const className = props.selectedTab === props.href ? 
-    "py-2 text-white md:py-0 text-2xl border-opacity-0 border-white border-b-4 border-opacity-100":
-    "py-2 text-white md:py-0 text-2xl border-opacity-0 border-white border-b-4 hover:border-opacity-100 transition ease-linear duration-300"
+    "py-2 text-white py-0 text-2xl border-opacity-0 border-white border-b-4 border-opacity-100":
+    "py-2 text-white py-0 text-2xl border-opacity-0 border-white border-b-4 hover:border-opacity-100 transition ease-linear duration-300"
     return (
     <div className="px-8 cursor-pointer">
         <Link 
@@ -22,7 +22,7 @@ export default function Header({ menuLinks, selectedTab }){
     selectedTab = selectedTab || null;
 
     return (
-        <header className="md:pt-8 pt-0 z-20 right-0 left-0 top-0 fixed transition-all ease-in-out box-border bg-gray-400 
+        <header className="md:pt-4 pt-0 z-20 right-0 left-0 top-0 fixed transition-all ease-in-out box-border bg-gray-400 
         bg-opacity-20 backdrop-filter backdrop-blur">
             <div className="max-w-7xl flex items-center mx-auto box-border justify-between">
                 {/* <div className="cursor-pointer box-border h-5 text-white text-lg z-10 p-0 top-0">Home</div> */}
